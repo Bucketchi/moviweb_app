a58f935ca7c781e45d4367164047e15f5f81e322
Bucketchi <alexandretcoutinho@gmail.com>
2023-09-26 17:00:03 +0100

    Added 404 error message

--- app.py
+++ app.py
@@ -82,5 +82,10 @@ def delete_movie(user_id, movie_id):
     return redirect(f'/users/{user_id}')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
